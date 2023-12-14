95745621c4afe34a124d84b792c86620fb27ff4f
Yip <yipzou@connect.hku.hk>
2023-12-14 16:45:09 +0800

    plot更新成交互式

--- lib/helper/plot_utils.py
+++ lib/helper/plot_utils.py
@@ -4,78 +4,99 @@ from sklearn.isotonic import IsotonicRegression
 import matplotlib.pyplot as plt
 from matplotlib.figure import Figure
 from typing import List
-from datetime import datetime
-
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 from ..evaluate.PolicyGenerator import PolicyGenerator
-
-def plot_pg_res(pg: PolicyGenerator, last_n: int = -1): 
+def plot_pg_res(pg: PolicyGenerator):
     df = pg.best_bt.move_df.copy()
-    if last_n > 0:
-        df = df.iloc[-last_n:]
-
-    if isinstance(df.index[0], str):
-        df.index = df.index.map(lambda x: datetime.strptime(x, "%Y-%m-%d"))
-    
     pnl_list = pd.Series(pg.best_result["pnl_list"], 
-                         index = pg.best_result["s_date"]) 
-    pnl_list.index = pnl_list.index.map(lambda x: pd.to_datetime(x))
+                            index = pg.best_result["s_date"],
+                            name = "pnl") 
+    if isinstance(pnl_list.index[0], str):
+        pnl_list.index = pnl_list.index.map(lambda x: pd.to_datetime(x))
     df["pnl"] = pnl_list
     df["pnl"] = df["pnl"].fillna(0)
     df["pnl"] = df["pnl"].cumsum()
-    fig, (ax1, ax2, ax3) = plt.subplots(3, 1, figsize=(24, 12), sharex=True)
 
-    ax1.plot(df[pg._col], label='Return', color='blue')
-    ax1.plot(df[f"stop_earning_bar"], label='stop_earning_bar', color='grey', linestyle = "--", alpha = 0.7)
-    ax1.plot(df[f"stop_loss_bar"], label='stop_loss_bar', color='grey', linestyle = "--", alpha = 0.7)
+    fig = make_subplots(rows=3, cols=1, shared_xaxes=True)
+    b_date = [pd.to_datetime(x) for x in pg.best_result["b_date"]]
+    s_date = [pd.to_datetime(x) for x in pg.best_result["s_date"]]
+    sl_date = [pd.to_datetime(x) for x in pg.best_result["stop_loss_date"]]
+    se_date = [pd.to_datetime(x) for x in pg.best_result["stop_earning_date"]]
 
-    ax2.plot(df[pg.dcol], label='Noise', color='orange', lw = 2)
-    ax2.plot(df[f"u_bound"], label='u_bound', color='grey', linestyle = "--", alpha = 0.7)
-    ax2.plot(df[f"l_bound"], label='l_bound', color='grey', linestyle = "--", alpha = 0.7)
-    
-    ax3.plot(df["pnl"], label='PnL', color='green')
-    ax3.plot(df["pnl"] - df[pg._col], label='excess return', color='pink')
-    
-    """Anotate buy and sell points"""
-    for date in pg.best_result["b_date"]: 
-        date = pd.to_datetime(date)
-        if date < df.index[0]:
-            continue
-        ax1.scatter(date, df[pg._col][date], c='red', marker='o')
-        ax2.scatter(date, df[pg.dcol][date], c='red', marker='o')
+    """Figure 1"""
+    fig.add_trace(go.Scatter(x = df.index, y = df[pg._col].values, 
+                            mode='lines', name = pg._col,
+                            line = dict(color='blue')
+                            ), row=1, col=1)
+    fig.add_trace(go.Scatter(x = df.index, y = df[f"stop_earning_bar"].values, 
+                            mode='lines', name='stop_earning_bar',
+                            line = dict(color='grey', dash='dash')
+                            ), row=1, col=1)
+    fig.add_trace(go.Scatter(x = df.index, y = df[f"stop_loss_bar"].values, 
+                            mode='lines', name='stop_loss_bar',
+                            line = dict(color='grey', dash='dash')
+                            ), row=1, col=1)
+    fig.add_trace(go.Scatter(x = b_date, y = df.loc[b_date, pg._col].values,
+                            mode='markers', name='buy',
+                            marker = dict(size = 10, color = "red")
+                            ), row=1, col=1)  # buy
+    fig.add_trace(go.Scatter(x = s_date, y = df.loc[s_date, pg._col].values,
+                            mode='markers', name='sell',
+                            marker = dict(size = 10, color = "green")
+                            ), row=1, col=1)  # sell
+    fig.add_trace(go.Scatter(x = se_date, y = df.loc[se_date, pg._col].values,
+                            mode='markers', name='stop earning',
+                            marker = dict(size = 10, color = "orange")
+                            ), row=1, col=1)  # stop earning
+    fig.add_trace(go.Scatter(x = sl_date, y = df.loc[sl_date, pg._col].values,
+                            mode='markers', name='stop loss',
+                            marker = dict(size = 10, color = "blue")
+                            ), row=1, col=1)  # stop loss
 
-    for date in pg.best_result["s_date"]: 
-        date = pd.to_datetime(date)
-        if date < df.index[0]:
-            continue
-        ax1.scatter(date, df[pg._col][date], c='green', marker='o')
-        ax2.scatter(date, df[pg.dcol][date], c='green', marker='o')
-    
-    for date in pg.best_result["stop_loss_date"]: 
-        date = pd.to_datetime(date)
-        if date < df.index[0]:
-            continue
-        ax1.scatter(date, df[pg._col][date], c='blue', marker='o')
-        ax2.scatter(date, df[pg.dcol][date], c='blue', marker='o')
-    
-    for date in pg.best_result["stop_earning_date"]: 
-        date = pd.to_datetime(date)
-        if date < df.index[0]:
-            continue
-        ax1.scatter(date, df[pg._col][date], c='orange', marker='o')
-        ax2.scatter(date, df[pg.dcol][date], c='orange', marker='o')
+    """Figure 2"""
+    fig.add_trace(go.Scatter(x = df.index, y = df[pg.dcol].values, 
+                            mode='lines', name = pg.dcol,
+                            line = dict(color='orange')
+                            ), row=2, col=1)
+    fig.add_trace(go.Scatter(x = df.index, y = df[f"u_bound"].values, 
+                            mode='lines', name='u_bound',
+                            line = dict(color='grey', dash='dash')
+                            ), row=2, col=1)
+    fig.add_trace(go.Scatter(x = df.index, y = df[f"l_bound"].values, 
+                            mode='lines', name='l_bound',
+                            line = dict(color='grey', dash='dash')
+                            ), row=2, col=1)
+    fig.add_trace(go.Scatter(x = b_date, y = df.loc[b_date, pg.dcol].values,
+                            mode='markers', name='buy',
+                            marker = dict(size = 10, color = "red")
+                            ), row=2, col=1)  # buy
+    fig.add_trace(go.Scatter(x = s_date, y = df.loc[s_date, pg.dcol].values,
+                            mode='markers', name='sell',
+                            marker = dict(size = 10, color = "green")
+                            ), row=2, col=1)  # sell
+    fig.add_trace(go.Scatter(x = se_date, y = df.loc[se_date, pg.dcol].values,
+                            mode='markers', name='stop earning',
+                            marker = dict(size = 10, color = "orange")
+                            ), row=2, col=1)  # stop earning
+    fig.add_trace(go.Scatter(x = sl_date, y = df.loc[sl_date, pg.dcol].values,
+                            mode='markers', name='stop loss',
+                            marker = dict(size = 10, color = "blue")
+                            ), row=2, col=1)  # stop loss
 
+    """Figure 3"""
+    fig.add_trace(go.Scatter(x = df.index, y = df["pnl"], 
+                            mode='lines', name = "PnL",
+                            line = dict(color='orange')
+                            ), row=3, col=1)
 
-    # 设置子图标题和标签
-    for ax in [ax1, ax2, ax3]:
-        ax.legend()
-        ax.grid()
-
-    ax1.set_title('Return Over Time')
-    ax2.set_title('Detrend Return Over Time')
-    ax3.set_title(f"PnL Over Time:\
-                  a: {pg.best_result['a']}, b: {pg.best_result['b']}, return: {pg.best_result['return']}%\n\
-                    Winning rate: {pg.best_result['winning_rate']}%, avg hold period: {pg.best_result['avg_hold_p']}days, avg return: {pg.best_result['avg_return']*100}%\n\
-                        Annualized Return: {pg.best_result['ann_return']*100}%")
+    fig.update_layout(
+                    xaxis_title='x',
+                    yaxis_title='y',
+                    hovermode='x',
+                    legend = dict(x=1, y=1),
+                    width = 800, height = 1500)
+    fig.show()
     return fig
 
 def plot_detrend_perform(col: str, 
